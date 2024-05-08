6e0ca83eebc8fd2d8560cc43ff12fe548352a4df
pwojton <piotr.wojton.01@gmail.com>
2024-05-08 16:57:54 +0200

    Add fetching number of recipes and ingredients from request

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 load_dotenv()
@@ -9,7 +10,17 @@ API_KEY = os.getenv('API_KEY')
 
 @app.route('/recipes', methods=['GET'])
 def get_recipes():
-    return 'Get all recipes'
+    ingredients = request.args.get('ingredients')
+    number_of_recipes = request.args.get('number-of-recipes', type=int)
+    
+    if not ingredients:
+        return jsonify({"error": "No ingredients provided"}), 400
+    
+    if not number_of_recipes:
+        return jsonify({"error": "No number of recipes provided"}), 400
+    
+    
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
