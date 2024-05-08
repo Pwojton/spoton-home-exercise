33989704b4dd44088189c31095fd9dc01f1752f5
pwojton <piotr.wojton.01@gmail.com>
2024-05-08 22:47:20 +0200

    Add deleting ingredients

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,13 +13,24 @@ const App: React.FC = () => {
   };
 
   const handleAddIngredient = (): void => {
-    if (currentIngredient.trim() !== "") {
+    const trimmedIngredient = currentIngredient.trim();
+    if (
+      trimmedIngredient !== "" &&
+      ingredients.length < 10 &&
+      !ingredients.includes(trimmedIngredient)
+    ) {
       setIngredients((prevIngredients) => [
         ...prevIngredients,
-        currentIngredient,
+        trimmedIngredient,
       ]);
       setCurrentIngredient("");
     }
+  };
+
+  const handleDeleteIngredient = (ingredientToDelete: string): void => {
+    setIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient !== ingredientToDelete)
+    );
   };
 
   const handleSubmit = (): void => {
@@ -51,24 +62,38 @@ const App: React.FC = () => {
           Add
         </Button>
       </Box>
-      <div>
-        <div className="ingredient">{ingredients.join(", ")}</div>
-      </div>
+      {ingredients.length === 10 && (
+        <Box sx={{ mt: 1 }}>
+          You have reached the maximum number of ingredients.
+        </Box>
+      )}
+      <Box sx={{ display: "inline-flex" }}>
+        {ingredients.map((ingredient, index) => (
+          <div
+            key={index}
+            className="ingredient"
+            onClick={() => handleDeleteIngredient(ingredient)}
+            style={{ cursor: "pointer", padding: "5px" }}
+          >
+            {ingredient}
+          </div>
+        ))}
+      </Box>
       {ingredients.length > 0 && (
         <Box sx={{ mt: 6 }}>
           <TextField
             id="recipes-number-input"
-            label="recipes number"
+            label="Recipes number"
             type="text"
             autoComplete="off"
-            sx={{ width: "110px" }}
-            inputProps={{ type: "number" }}
+            sx={{ width: "145px" }}
+            inputProps={{ type: "number", min: 1, max: 10 }}
           />
           <Button
             variant="contained"
             size="large"
             sx={{ mt: 0.8, ml: 4 }}
-            onClick={handleAddIngredient}
+            onClick={handleSubmit}
           >
             Submit
           </Button>
