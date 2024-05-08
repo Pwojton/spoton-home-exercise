17d3158c6b8283c3469e40c0a06e27b9fb644f30
pwojton <piotr.wojton.01@gmail.com>
2024-05-08 21:52:57 +0200

    Add simple form for adding ingredients

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,74 @@
+import React, { useState, ChangeEvent } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import "./styles/App.css";
 
-function App() {
-  return <div className="App">Test</div>;
-}
+const App: React.FC = () => {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [currentIngredient, setCurrentIngredient] = useState<string>("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setCurrentIngredient(event.target.value);
+  };
+
+  const handleAddIngredient = (): void => {
+    if (currentIngredient.trim() !== "") {
+      setIngredients((prevIngredients) => [
+        ...prevIngredients,
+        currentIngredient,
+      ]);
+      setCurrentIngredient("");
+    }
+  };
+
+  const handleSubmit = (): void => {
+    console.log("Submitted Ingredients:", ingredients);
+  };
+
+  return (
+    <div className="App">
+      <h1>Recipes generator</h1>
+      <Box
+        component="form"
+        sx={{ mt: 5 }}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()}
+      >
+        <TextField
+          id="ingredient-input"
+          label="Ingredient"
+          type="text"
+          autoComplete="off"
+          className="ingredient-input"
+          value={currentIngredient}
+          onChange={handleInputChange}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ mt: 0.8, ml: 4 }}
+          onClick={handleAddIngredient}
+        >
+          Add
+        </Button>
+      </Box>
+      <div>
+        <div className="ingredient">{ingredients.join(", ")}</div>
+      </div>
+      {ingredients.length > 0 && (
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            sx={{ mt: 6 }}
+          >
+            Submit
+          </Button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
