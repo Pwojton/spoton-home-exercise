5d534db6aa197ce847d66f432e3875fd15922cb8
pwojton <piotr.wojton.01@gmail.com>
2024-05-08 16:42:57 +0200

    Add importing spooncoolar API key from env file

--- server/app.py
+++ server/app.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 @app.route('/recipes', methods=['GET'])
 def get_recipes():
-    pass
+    return 'Get all recipes'
 
 if __name__ == '__main__':
     app.run(debug=True)
